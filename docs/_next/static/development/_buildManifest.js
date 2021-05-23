49decb85a7c53f85b3df5403af65f34165393857
DarkLegend360 <darklegend360@live.com>
2021-05-23 21:15:56 +0530

    Fixed Bugs

--- docs/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/sorting":["static\u002Fchunks\u002Fpages\u002Fsorting.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fsorting"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
